75c5a103211ad17997411d3c649514ab57a2c03d
nulqqqqq <94065421+nulqqqqq@users.noreply.github.com>
2023-01-30 16:16:43 +0300

    Delete diplom directory

--- diplom/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace diplom.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
